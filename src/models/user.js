d8e4ec2fd7a6086591744ed56eb65ad912d79d6f
Harsharma18 <ns750759@gmail.com>
2025-08-14 19:39:39 +0530

    feat(models): add User model schema

---
+++ src/models/user.js
@@ -0,0 +1,94 @@
+// models/User.js
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpiry: {
+      type: Date,
+    },
+    otpVerified: {
+  type: Boolean,
+  default: false,
+},
+
+    age: { 
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
+    },
+    profileImg: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+userSchema.methods.comparepassword = async function (candidatepassword) {
+  const password = await bcrypt.compare(candidatepassword, this.password);
+  return password;
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
